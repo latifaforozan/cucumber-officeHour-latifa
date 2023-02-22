0475f8ab65769b6e5d8d5748195d088a641a4997
Latifa forozan <la.forozan@gmail.com>
2023-02-22 11:35:04 -0800

    new codes added

---
+++ src/test/java/com/cydeo/pages/ButtonPage.java
@@ -0,0 +1,15 @@
+package com.cydeo.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import java.util.List;
+
+// page_url = https://demoqa.com/buttons
+public class ButtonPage {
+
+    public ButtonPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+}
